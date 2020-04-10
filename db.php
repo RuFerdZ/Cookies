59db956527b67f638b92fe4dc7202743d20ed2f6
Isala Piyarisi <code@isala.me>
2020-04-10 11:23:49 +0530

    Added db connection file

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+	$dbuser = 'cookies';
+	$dbpass = 'NoCookies4U';
+    try {
+        $dbh = new PDO('mysql:host=128.199.253.218;dbname=CookieJar', $dbuser, $dbpass);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die('Connection failed: ' . $e->getMessage());
+    }
+    foreach ($dbh->query('SELECT * from dentist') as $row) {
+        print_r($row);
+    }
+?>
