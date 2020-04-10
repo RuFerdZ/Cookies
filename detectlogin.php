cbaaa8b7c415d25d23215e735243589c90237086
Tony <50706244+B-127@users.noreply.github.com>
2020-04-10 16:16:12 +0530

    Update detectlogin.php

--- detectlogin.php
+++ detectlogin.php
@@ -3,7 +3,7 @@ if(isset($_SESSION['userid'])){
     $fName = $_SESSION['fname'];
     $sName = $_SESSION['sname'];
     $userId = $_SESSION['userid'];
-    $userType = $_SESSION['user_type'];
+    $userType = $_SESSION['usertype'];
     echo "<div style = 'float: right'>";
     echo "<span style = 'text-align: right'> $fName $sName / $userType No: $userId </span>";
     echo "</div>";
